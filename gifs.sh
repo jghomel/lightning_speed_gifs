e30f8c0dbd673d8c8163a40e35732c515c6cd3be
Jacob <jghomel@gmail.com>
2017-11-24 09:37:40 -0500

    Add files via upload

---
+++ gifs.sh
@@ -0,0 +1,18 @@
+#Dependcies: 
+#youtube-dl
+#ffmpeg 
+#drop to gif 
+
+open /Applications/Drop\ to\ GIF.app
+echo Enter youtube link:
+read yt_link
+echo Enter start time HH:MM:SS[.xxx]:
+read start_time
+echo Enter desired duration HH:MM:SS[.xxx]:
+read end_time
+echo enter desired quality among following:
+youtube-dl -F $yt_link
+read quality
+
+final_link="$(youtube-dl -f $quality -g $yt_link)"
+ffmpeg -ss "$start_time" -i "$final_link" -to "$end_time" -c copy ~/Documents/automatic_gif_repo/extracted.mp4
